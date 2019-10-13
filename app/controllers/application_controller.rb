61db25ddc135826d1ca29280074f70cfbfc2e8c9
Danil <danilsyrovatskii@gmail.com>
2019-10-13 17:19:33 +0300

    add authorization output when going to the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   def home
     #debugger
 
-
+    if current_user
+    
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
