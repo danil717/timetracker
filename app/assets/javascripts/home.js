aa4e43269d8920af621aa43d495908c5512c05e6
Alex <lesadang1@gmail.com>
2019-10-17 17:10:55 +0300

    hidden functionality

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,20 @@
+document.addEventListener('turbolinks:load', function() {
+  let form = null;
+  document.addEventListener("start", function(event) {
+    let hiddens = form.getElementsByClassName('hidden_btn')
+    for(let i of hiddens) {
+      i.hidden = false;
+      i.setAttribute("task__id", event.detail.taskId);
+    }
+
+    form.start.hidden = true;
+  });
+
+  inputs.onclick = function(e) {
+    if(e.target.id == 'start') {
+      form = e.target.form;
+      // disable button
+      form.submit.click();
+    }
+  }
+});
