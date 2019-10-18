16cad90b9e1928440e2cc413fb893ab671492f19
Alex <lesadang1@gmail.com>
2019-10-18 13:37:04 +0300

    disabled functionality

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,13 @@
 document.addEventListener('turbolinks:load', function() {
   let form = null;
+
+
   document.addEventListener("start", function(event) {
-    let hiddens = form.getElementsByClassName('hidden_btn')
+    let hiddens = form.getElementsByClassName('hidden_btn');
     for(let i of hiddens) {
       i.hidden = false;
       i.setAttribute("task__id", event.detail.taskId);
     }
-
     form.start.hidden = true;
   });
 
@@ -14,7 +15,14 @@ document.addEventListener('turbolinks:load', function() {
     if(e.target.id == 'start') {
       form = e.target.form;
       // disable button
+
+      //
       form.submit.click();
-    }
-  }
+      let btn = document.getElementsByClassName('btn-success');
+      for(let i of btn) {
+        i.disabled = true;
+      };
+    };
+  };
+
 });
