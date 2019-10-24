4bed816c322d74c42a349ee5c762dc1de2c55453
Alex <lesadang1@gmail.com>
2019-10-24 18:27:34 +0300

    my first commit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,25 +3,22 @@ document.addEventListener('turbolinks:load', function() {
 
   document.addEventListener("start", function(event) {
     let hiddens = form.getElementsByClassName('hidden_btn');
+    let addTask = document.getElementById('add__task');
+
+    addTask.disabled = false;
     for(let i of hiddens) {
+      i.disabled = false
       i.hidden = false;
       i.setAttribute("task__id", event.detail.taskId);
     }
     form.start.hidden = true;
-    // let pause = form.getElementById('pause') не находит по ИД
-    // let stop = form.getElementById('stop')
+
     let pause = form.getElementsByClassName('btn-warning')[0];
     let stop = form.getElementsByClassName('btn-danger')[0];
+    let start = form.getElementsByClassName('btn-success')[0]
 
     function stopPause() {
       let taskId = pause.getAttribute('task__id');
-      // var req = new XMLHttpRequest();
-      // req.open('POST', `/tasks/${taskId}/completion`, true);
-      // req.setRequestHeader("Accept", "text/javascript");
-      // req.responseType='script';
-      // req.send();
-
-
 
       const response = fetch(`/tasks/${taskId}/completion`, {
         method: 'post',
@@ -31,44 +28,61 @@ document.addEventListener('turbolinks:load', function() {
       })
       .then(response => response.text())
       .then(data => {
-        console.log('Request succeeded with JSON response', data);
-        let obj = JSON.parse(data)
-        let td1 = document.createElement('td');
-        let td2 = document.createElement('td');
-        let td3 = document.createElement('td');
-        let td4 = document.createElement('td');
-        let tr = document.createElement('tr');
-        let table = document.getElementsByClassName('table')[0]
-        table.appendChild(tr)
-        td1.innerText = obj.user_id;
-        td2.innerText = obj.description;
-        td3.innerText = 456789
-        td4.innerText = 'asdadafdsaf';
-        tr.appendChild(td1)
-        tr.appendChild(td2)
-        tr.appendChild(td3)
-        tr.appendChild(td4)
+        addTaskToTable(data);
       })
       .catch(function (error) {
         console.log('Request failed', error);
       });
     };
+
     pause.addEventListener('click', stopPause); //() => stopPause(pause.getAttribute('task__id'))
     stop.addEventListener('click', stopPause);
+
+    pause.onclick = function() {
+      start.hidden = false
+      start.disabled = false
+      this.hidden = true
+    }
+
+    stop.onclick = function () {
+      form.remove();
+    }
   });
+
   if(window.location.href === "http://localhost:3000/") {
     inputs.onclick = function(e) {
       if(e.target.id == 'start') {
         form = e.target.form;
-        // disable button
 
-        //
         form.submit.click();
-        let btn = document.getElementsByClassName('btn-success');
+        let btn = document.getElementsByClassName('btn');
+        let description = document.getElementById('description');
         for(let i of btn) {
           i.disabled = true;
         };
+        description.disabled = true;
       };
     };
   };
+
+  function addTaskToTable(data) {
+    console.log(data);
+    let obj = JSON.parse(data)
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+    let tr = document.createElement('tr');
+    let table = document.getElementsByClassName('table')[0];
+
+    table.appendChild(tr)
+    td1.innerText = obj.user_id;
+    td2.innerText = obj.description;
+    td3.innerText = 456789
+    td4.innerText = 'asdadafdsaf';
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+  }
 });
