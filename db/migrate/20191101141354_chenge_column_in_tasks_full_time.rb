4a0373047ff067082cde94cfb5426b2982f7c8c5
Danil <danilsyrovatskii@gmail.com>
2019-11-01 16:15:59 +0200

    fix bag

--- db/migrate/20191101141354_chenge_column_in_tasks_full_time.rb
+++ db/migrate/20191101141354_chenge_column_in_tasks_full_time.rb
@@ -1,9 +1,6 @@
 class ChengeColumnInTasksFullTime < ActiveRecord::Migration[5.2]
-  def up
+  def change
     remove_column :tasks, :end_time
-  end
-
-  def down
     add_column :tasks, :end_time, :datetime
   end
 end
